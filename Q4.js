06e40ff45179a5eb05bddfa699f62b9a05df0005
vampyteuth <94151404+vampyteuth@users.noreply.github.com>
2022-03-24 23:12:15 -0800

    Created Q4

---
+++ Q4.js
@@ -0,0 +1,9 @@
+// 4Sort in decreasing order
+// importance: 4
+let arr = [5, 2, 1, -10, 8];
+
+arr.sort((a, b)=> b-a)
+
+console.log (arr);
+// ... your code to sort it in decreasing order b-a*
+alert( arr ); // 8, 5, 2, 1, -10
