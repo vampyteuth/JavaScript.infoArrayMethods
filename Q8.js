9e866a34f9951286b78526d291e24bf0818b84b4
vampyteuth <94151404+vampyteuth@users.noreply.github.com>
2022-03-25 21:08:58 -0800

    Created Q8

---
+++ Q8.js
@@ -0,0 +1,33 @@
+
+
+// //8Map to objects
+// importance: 5
+// You have an array of user objects, each one has name, surname and id.
+
+// Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+// For instance:
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+let usersMapped = users.map(user => ({
+    fullName: `${user.name} ${user.surname}`,
+    id: user.id
+}))
+
+// /*
+// usersMapped = [
+//   { fullName: "John Smith", id: 1 },
+//   { fullName: "Pete Hunt", id: 2 },
+//   { fullName: "Mary Key", id: 3 }
+// ]
+// */
+    console.log(usersMapped)
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // John Smith
+// So, actually you need to map one array of objects to another. Try using => here. There’s a small catch. you need extra set of () 
+//where the computer thinks its running a function but it's actually an object
